5d6d1f3ada5778ae62d08007a6a43f64a7798a71
lovelyProject <edward.hardwork2000@gmail.com>
2023-04-26 10:04:42 +0500

    made construction for checking form

--- dir1/dir2/index.php
+++ dir1/dir2/index.php
@@ -1,31 +1,37 @@
 <?php 
     session_start();
 
-    if (!empty($_POST)) {
-        $host = "localhost";
-        $user = "root";
-        $pass = "root";
-        $name = "test";
-        var_dump($_POST);
-        $link = mysqli_connect($host, $user, $pass, $name);
-        mysqli_query($link, "SET NAMES 'utf8'");
-
-
-        $name = $_POST['name'];
-        $age = $_POST['age'];
-        $salary = $_POST['salary'];
-
-        $query = "INSERT INTO users (name, age, salary) VALUES ('$name', '$age', '$salary')";
-        $result = mysqli_query($link,  $query) or die(mysqli_error($link));
-
-        $_SESSION['flash'] = "success flash message";
-        header("Location: index.php");
-    } else
 
     if (isset($_SESSION['flash'])) {
         echo $_SESSION['flash'];
         unset($_SESSION['flash']);
     }
+
+    if (!empty($_POST)) {
+        $formName = $_POST['name'];
+        $formAge = $_POST['age'];
+        $formSalary = $_POST['salary'];
+
+        if (is_int($formAge) && is_int($formSalary) && is_string($formName)) {
+            $host = "localhost";
+            $user = "root";
+            $pass = "root";
+            $name = "test";
+
+            $link = mysqli_connect($host, $user, $pass, $name);
+            mysqli_query($link, "SET NAMES 'utf8'");
+
+            $query = "INSERT INTO users (name, age, salary) VALUES ('$formName', '$formAge', '$formSalary')";
+            $result = mysqli_query($link, $query);
+
+            $_SESSION['flash'] = "форма прошла валидацию и успешно сохранена";
+            header("Location: index.php");
+        } else {
+            $_SESSION['flash'] =  "форма не прошла валидацию";
+        }
+    }
+
+
 ?>
 
 <!DOCTYPE html>
@@ -39,8 +45,8 @@
 <body>
     <form method="POST">
         <input type="text" name="name" placeholder="name">
-        <input type="text" name="age" placeholder="age">
-        <input type="text" name="salary" placeholder="salary">
+        <input type="number" name="age" placeholder="age">
+        <input type="number" name="salary" placeholder="salary">
         <input type="submit">
     </form>
 </body>
