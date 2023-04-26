caddcc05b4582bc77fab775338286beccf6ccc5c
lovelyProject <edward.hardwork2000@gmail.com>
2023-04-26 10:21:18 +0500

    added saving forms values

--- dir1/dir2/index.php
+++ dir1/dir2/index.php
@@ -1,18 +1,12 @@
 <?php 
     session_start();
-
-
-    if (isset($_SESSION['flash'])) {
-        echo $_SESSION['flash'];
-        unset($_SESSION['flash']);
-    }
-
+    var_dump($_POST);
     if (!empty($_POST)) {
         $formName = $_POST['name'];
         $formAge = $_POST['age'];
         $formSalary = $_POST['salary'];
 
-        if (is_int($formAge) && is_int($formSalary) && is_string($formName)) {
+        if (is_int((int)$formAge) && is_int((int)$formSalary) && is_string((string)$formName)) {
             $host = "localhost";
             $user = "root";
             $pass = "root";
@@ -25,13 +19,16 @@
             $result = mysqli_query($link, $query);
 
             $_SESSION['flash'] = "форма прошла валидацию и успешно сохранена";
-            header("Location: index.php");
+            // header("Location: index.php");
         } else {
             $_SESSION['flash'] =  "форма не прошла валидацию";
         }
     }
 
-
+    if (isset($_SESSION['flash'])) {
+        echo $_SESSION['flash'];
+        unset($_SESSION['flash']);
+    }
 ?>
 
 <!DOCTYPE html>
@@ -43,10 +40,10 @@
     <title>Document</title>
 </head>
 <body>
-    <form method="POST">
-        <input type="text" name="name" placeholder="name">
-        <input type="number" name="age" placeholder="age">
-        <input type="number" name="salary" placeholder="salary">
+    <form method="POST" action="index.php">
+        <input type="text" name="name" placeholder="name" value="<?= $_POST['name']?>">
+        <input type="number" name="age" placeholder="age" value="<?= $_POST['age'] ?>">
+        <input type="text" name="salary" placeholder="salary" value="<?= $_POST['salary'] ?>">
         <input type="submit">
     </form>
 </body>
