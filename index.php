d79a6518cf79ad9df3aa823fe08cb84ee1f44e8e
lovelyProject <edward.hardwork2000@gmail.com>
2023-05-03 09:38:07 +0500

    made understandable url with db

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
 <?php 
 $link = require './connect.php';
+$layout = file_get_contents("./layout.php");
 $url = $_SERVER['REQUEST_URI'];
 
-echo $layout;
+// echo $layout;
+// echo $url;
+preg_match("#/page/([a-zA-Z0-9_-]+)#", $url, $match);
+$slug = $match[1];
+
+$query = "SELECT * FROM pages WHERE slug = '$slug'";
+$result = mysqli_query($link, $query);
+$page = mysqli_fetch_assoc($result);
+// var_dump($page);
+if (!empty($page)) {
+    $layout = preg_replace("{{{ content }}}", $page['content'], $layout);
+    $layout = preg_replace("{{{ title }}}", $page['slug'], $layout);
+
+    echo $layout;
+}
 ?>
+
