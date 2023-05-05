4fd70c9f52419863b766646eb6261d6946632e17
lovelyProject <edward.hardwork2000@gmail.com>
2023-05-05 09:28:16 +0500

    send post pararms in json format

--- file.php
+++ file.php
@@ -1,6 +1,6 @@
 <?php 
 	if (!empty($_POST)) {
-		echo $_POST['month'];
+		echo array_sum(json_decode($_POST['json']));
 	} else {
 		echo 'error';
 	}
--- json.php
+++ json.php
@@ -6,7 +6,9 @@ curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($curl, CURLOPT_POST, 1);
 
-$data = ['month' => "May"];
+$arr = [1,2,3,4,5];
+$json = json_encode($arr);
+$data = ['json' => $json];
 
 curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
 
